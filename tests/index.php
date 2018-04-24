52fc379c777d5253ac7f5d87d2e9713ff782905f
Cedric Lavery <cedriko-34@live.fr>
2018-04-24 15:02:29 +0200

    inclure des portions de page

---
+++ tests/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <meta charset="utf-8" />
+
+    <title>Mon super site</title>
+
+</head>
+
+
+
+<body>
+
+
+
+<!-- Insère le contenu de la page header.php -->
+
+
+
+<?php include("header.php"); ?>
+
+
+
+<!-- Insère le contenu de la page menu.php -->
+
+
+
+<?php include("menus.php"); ?>
+
+
+
+<!-- Le corps -->
+
+
+
+<div id="corps">
+
+    <h1>Mon super site</h1>
+
+
+
+    <p>
+
+        Bienvenue sur mon super site !<br />
+
+        Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
+
+    </p>
+
+</div>
+
+
+
+<!-- Insère le contenu de la page footer.php -->
+
+
+
+<?php include("footer.php")?>
+
+
+
+</body>
+
+</html>
---
+++ tests/menus.php
@@ -0,0 +1,10 @@
+<nav id="menu">
+    <div class="element_menu">
+        <h3>Titre menu</h3>
+        <ul>
+            <li><a href="">Lien</a></li>
+            <li><a href="">Lien</a></li>
+            <li><a href="">Lien</a></li>
+        </ul>
+    </div>
+</nav>
